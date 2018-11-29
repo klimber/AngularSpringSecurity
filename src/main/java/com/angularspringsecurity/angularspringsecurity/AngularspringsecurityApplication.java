f364fc2db0dcba558ef0ed0c0639be4bb479a240
Allan Krueger <klimber.mail@gmail.com>
2018-11-29 10:20:07 -0200

    Initial Commit

---
+++ src/main/java/com/angularspringsecurity/angularspringsecurity/AngularspringsecurityApplication.java
@@ -0,0 +1,12 @@
+package com.angularspringsecurity.angularspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularspringsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularspringsecurityApplication.class, args);
+	}
+}
